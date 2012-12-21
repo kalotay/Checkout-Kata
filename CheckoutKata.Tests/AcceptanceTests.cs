5c701f1bbd989a989026a5275742d963db0fd643
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-21 10:52:14 +0000

    first test

---
+++ CheckoutKata.Tests/AcceptanceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CheckoutKata.Tests
+{
+    [TestFixture]
+    public class AcceptanceTests
+    {
+        [Test]
+        public void GivenNoItemsPriceSHouldBeZero()
+        {
+            var checkout = 0;
+
+            Assert.That(checkout, Is.EqualTo(0));
+        }
+    }
+}
