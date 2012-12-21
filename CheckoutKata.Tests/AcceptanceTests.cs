5119fc0eda28a49069c5ae8a4b155beb19a791a7
Daniel Kalotay <daniel.kalotay@7digital.com>
2012-12-21 10:56:44 +0000

    added test for single item

--- CheckoutKata.Tests/AcceptanceTests.cs
+++ CheckoutKata.Tests/AcceptanceTests.cs
@@ -12,5 +12,24 @@ namespace CheckoutKata.Tests
 
             Assert.That(checkout, Is.EqualTo(0));
         }
+
+        [TestCase("A", 50)]
+        public void GivenOneItemShouldReturnItsPrice(object item, int price)
+        {
+            var checkout = new Checkout();
+            checkout.Add(item);
+
+            Assert.That(checkout.Price, Is.EqualTo(price));
+        }
+    }
+
+    public class Checkout
+    {
+        public void Add(object item)
+        {
+            if (item.Equals("A")) Price = 50;
+        }
+
+        public int Price { get; set; }
     }
 }
